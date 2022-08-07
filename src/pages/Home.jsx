70cbd91ac3d430633800a09c24bdb6114fd7bcdf
Anurag Pramanik <p.anurag@outlook.com>
2022-08-07 15:24:38 +0530

    Remove: redux and redux-toolkit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,28 @@ const Home = () => {
     <>
       <section className='container'>
         <div className='flex items-center justify-between p-6 m-12'>
-          <div className='flex flex-col p-24 text-6xl text-blue-900'>
-            <h1>Hi There!</h1>
-            <span className='mt-6 text-4xl'>
+          <div className='flex flex-col p-24 text-4xl text-slate-700'>
+            <div>
+              <h1>
+                Hi There!{' '}
+                <span className='wave' role='img' aria-labelledby='wave'>
+                  👋
+                </span>
+              </h1>
+            </div>
+            <h2 className='mt-4'>
+              I'M <strong>ANURAG</strong>.
+            </h2>
+            <span className='mt-6 text-5xl text-blue-900'>
               <Type />
             </span>
           </div>
-          <img src='/public/home-right.png' alt='home-right' width='400' />
+          <img
+            src='/home-right.png'
+            alt='home-right'
+            width='400'
+            className='pl-4'
+          />
         </div>
       </section>
     </>
