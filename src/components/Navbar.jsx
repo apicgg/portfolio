d89f11e684a3c7f92426b6a03686be32c5fd7acc
Anurag Pramanik <p.anurag@outlook.com>
2022-08-13 23:04:32 +0530

    Fix: merge conflict

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,39 +15,35 @@ const Header = () => {
         </Link>
       </div>
 
-          {/* Menu Items */}
-          <ul className='flex items-center justify-between space-x-12 text-lg font-[500]'>
-            <li>
-              <Link
-                to='/'
-                className='flex items-center hover:text-blue-900 nav'>
-                <AiOutlineHome className='mr-1' /> Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to='/about'
-                className='flex items-center hover:text-blue-900 nav'>
-                <AiOutlineUser className='mr-1' /> About
-              </Link>
-            </li>
-            <li>
-              <Link
-                to='/project'
-                className='flex items-center hover:text-blue-900 nav'>
-                <AiOutlineFundProjectionScreen className='mr-1' /> Projects
-              </Link>
-            </li>
-            <li>
-              <Link
-                to='/resume'
-                className='flex items-center hover:text-blue-900 nav'>
-                <AiFillStar className='mr-1' /> Resume
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      {/* Menu Items */}
+      <ul className='flex items-center justify-between space-x-12 text-lg font-[500]'>
+        <li>
+          <Link to='/' className='flex items-center hover:text-blue-900 nav'>
+            <AiOutlineHome className='mr-1' /> Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            to='/about'
+            className='flex items-center hover:text-blue-900 nav'>
+            <AiOutlineUser className='mr-1' /> About
+          </Link>
+        </li>
+        <li>
+          <Link
+            to='/project'
+            className='flex items-center hover:text-blue-900 nav'>
+            <AiOutlineFundProjectionScreen className='mr-1' /> Projects
+          </Link>
+        </li>
+        <li>
+          <Link
+            to='/resume'
+            className='flex items-center hover:text-blue-900 nav'>
+            <AiFillStar className='mr-1' /> Resume
+          </Link>
+        </li>
+      </ul>
     </header>
   )
 }
