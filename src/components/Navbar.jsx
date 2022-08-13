5054cab727f7e174e364c2088305752145093041
Anurag Pramanik <p.anurag@outlook.com>
2022-08-13 23:06:24 +0530

    Fix Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,35 +15,30 @@ const Header = () => {
         </Link>
       </div>
 
-      {/* Menu Items */}
-      <ul className='flex items-center justify-between space-x-12 text-lg font-[500]'>
-        <li>
-          <Link to='/' className='flex items-center hover:text-blue-900 nav'>
-            <AiOutlineHome className='mr-1' /> Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            to='/about'
-            className='flex items-center hover:text-blue-900 nav'>
-            <AiOutlineUser className='mr-1' /> About
-          </Link>
-        </li>
-        <li>
-          <Link
-            to='/project'
-            className='flex items-center hover:text-blue-900 nav'>
-            <AiOutlineFundProjectionScreen className='mr-1' /> Projects
-          </Link>
-        </li>
-        <li>
-          <Link
-            to='/resume'
-            className='flex items-center hover:text-blue-900 nav'>
-            <AiFillStar className='mr-1' /> Resume
-          </Link>
-        </li>
-      </ul>
+      <nav className='main-nav'>
+        <ul className='main-nav-list'>
+          <li>
+            <Link to='/' className='main-nav-link'>
+              <AiOutlineHome className='mr-1' /> Home
+            </Link>
+          </li>
+          <li>
+            <Link to='/about' className='main-nav-link'>
+              <AiOutlineUser className='mr-1' /> About
+            </Link>
+          </li>
+          {/* <li>
+            <Link to='/project' className='main-nav-link'>
+              <AiOutlineFundProjectionScreen className='mr-1' /> Projects
+            </Link>
+          </li>
+          <li>
+            <Link to='/resume' className='main-nav-link'>
+              <AiFillStar className='mr-1' /> Resume
+            </Link>
+          </li> */}
+        </ul>
+      </nav>
     </header>
   )
 }
