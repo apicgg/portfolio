fde0bc2b09a50d9f1038fcaf74e362aaa33b5f46
Anurag Pramanik <p.anurag@outlook.com>
2022-08-19 16:46:58 +0530

    Todo: hamburger nav menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,26 +9,28 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <nav className='bg-slate-400 h-24'>
-      <header className='container mx-auto p-4 '>
+      <header className='container mx-auto md:pt-4 p-6 '>
         <div className='flex items-center justify-between'>
           <div>
-            <Link className='text-3xl text-blue-900 font-bold' to='/'>
+            <Link
+              className='text-xl md:text-3xl text-blue-900 font-semibold md:font-bold'
+              to='/'>
               AP.
             </Link>
           </div>
 
-          <ul className='flex items-center gap-12'>
+          <ul className='md:flex items-center gap-10 hidden'>
             <li>
               <Link
                 to='/'
-                className='flex items-center text-white font-medium text-lg py-2 px-6 rounded-lg bg-blue-900 hover:scale-110 transition-all duration-300'>
+                className='flex items-center text-white font-medium text-lg py-2 px-4 rounded-lg bg-blue-900 hover:scale-110 transition-all duration-300'>
                 <AiOutlineHome className='mr-1' /> Home
               </Link>
             </li>
             <li>
               <Link
                 to='/about'
-                className='flex items-center text-white font-medium text-lg py-2 px-6 rounded-lg bg-blue-900 hover:scale-110 transition-all duration-300'>
+                className='flex items-center text-white font-medium text-lg py-2 px-4 rounded-lg bg-blue-900 hover:scale-110 transition-all duration-300'>
                 <AiOutlineUser className='mr-1' /> About
               </Link>
             </li>
@@ -36,17 +38,22 @@ const Header = () => {
               <a
                 href='https://apicgg.github.io/digital-resume/'
                 target='_blank'
-                className='flex items-center text-white font-medium text-lg py-2 px-6 rounded-lg bg-blue-900 hover:scale-110 transition-all duration-300  main-nav-link-sec'>
+                className='flex items-center text-white font-medium text-lg py-2 px-4 rounded-lg bg-blue-900 hover:scale-110 transition-all duration-300  main-nav-link-sec'>
                 <AiFillStar className='mr-1' /> Resume
               </a>
             </li>
-            {/* <li>
-            <Link to='/project' className='main-nav-link'>
-              <AiOutlineFundProjectionScreen className='mr-1' /> Projects
-            </Link>
-          </li>
-           */}
           </ul>
+
+          {/* TODO */}
+          {/* Hamburger Icon */}
+          <button
+            // onClick={onClick}
+            id='menu-btn'
+            class='block hamburger md:hidden focus:outline-none'>
+            <span class='hamburger-top'></span>
+            <span class='hamburger-middle'></span>
+            <span class='hamburger-bottom'></span>
+          </button>
         </div>
       </header>
     </nav>
