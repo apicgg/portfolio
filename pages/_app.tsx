c9da404d41e241b5efea08c3f52c4c568a7ae5f7
Anurag Pramanik <p.anurag@outlook.com>
2022-11-02 10:57:24 +0530

    feat: add footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Header from 'components/Header'
+import Footer from 'components/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <main className='bg-backgroundColor text-fontColor'>
         <Header />
         <Component {...pageProps} />
+        <Footer />
       </main>
     </>
   )
