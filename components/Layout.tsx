7ab88a3fff4c85d41e89eee921b112e9148ed1ca
Anurag Pramanik <p.anurag@outlook.com>
2022-11-20 20:04:11 +0530

    feat(layout): add general layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const Layout = (props: Props) => {
+  return (
+    <div className='bg-backgroundColor text-fontColor'>
+      <Header />
+      <main>{props.children}</main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
