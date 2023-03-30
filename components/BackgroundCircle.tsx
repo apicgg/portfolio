3a388350f3d9cd0a6abd9d2688668c0bb1d76288
Anurag Pramanik <p.anurag@outlook.com>
2023-03-30 17:04:19 +0530

    Change the colorscheme

--- components/BackgroundCircle.tsx
+++ components/BackgroundCircle.tsx
@@ -20,7 +20,7 @@ const BackgroundCircle = () => {
       <div className="absolute border border-[#8a8a8a] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
       <div className="absolute border border-[#333] rounded-full h-[300px] w-[300px] mt-52" />
       <div className="absolute border border-[#333] rounded-full h-[500px] w-[500px] mt-52" />
-      <div className="absolute border rounded-full border-[#f7ab0a] opacity-20 h-[650px] w-[650px] mt-52 animate-pulse" />
+      <div className="absolute border rounded-full border-[#F9C34D] opacity-20 h-[650px] w-[650px] mt-52 animate-pulse" />
     </motion.div>
   );
 };
