e852e490b81cab8cacc2408d5885b6bf5c9d3180
Anurag Pramanik <p.anurag@outlook.com>
2023-10-20 09:38:20 +0530

    use next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { Inter } from "@next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 import Head from "next/head";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
