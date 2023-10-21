e8b0f79edbeed63c60fb216b029f132626c9ea28
Anurag Pramanik <p.anurag@outlook.com>
2023-10-21 12:02:36 +0530

    feat: add husky

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
-import { FaXTwitter } from 'react-icons/fa6';
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   let date = new Date();
