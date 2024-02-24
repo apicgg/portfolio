690af73f8b4de7fd20c22f88acdf8ca0f07da119
Anurag Pramanik <p.anurag@outlook.com>
2024-02-24 12:41:59 +0530

    feat: add blog page and conditional route from nav

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+const BlogPage = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">BlogPage</div>
+  );
+};
+
+export default BlogPage;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,17 @@
 
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import ThemeToggle from "./ThemeToggleButton";
 
 const Header = () => {
+  const path = usePathname();
+
+  const isBlogPath = path.includes("blog");
+
   return (
     <header className="fixed z-20 w-full backdrop-blur-md">
-      <div className="mx-auto flex max-w-5xl items-center justify-between p-2">
+      <nav className="mx-auto flex max-w-5xl items-center justify-between p-2">
         <motion.div
           initial={{
             x: -500,
@@ -32,25 +37,53 @@ const Header = () => {
           </Link>
         </motion.div>
 
-        <motion.ul
-          initial={{
-            x: 500,
-            opacity: 0,
-            scale: 0.5,
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-          }}
-          transition={{
-            duration: 1.5,
-          }}
-          className="flex items-center gap-2 lg:gap-10"
-        >
-          <ThemeToggle />
-        </motion.ul>
-      </div>
+        <div className="flex items-center justify-center gap-5">
+          <motion.ul
+            initial={{
+              x: 500,
+              opacity: 0,
+              scale: 0.5,
+            }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              scale: 1,
+            }}
+            transition={{
+              duration: 1.5,
+            }}
+            className="flex items-center gap-2 lg:gap-10"
+          >
+            <ThemeToggle />
+          </motion.ul>
+          <motion.ul
+            initial={{
+              x: 500,
+              opacity: 0,
+              scale: 0.5,
+            }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              scale: 1,
+            }}
+            transition={{
+              duration: 1.5,
+            }}
+            className="flex items-center gap-2 lg:gap-10"
+          >
+            {isBlogPath ? (
+              <Link href="/" prefetch={true} className="coolButton">
+                Home
+              </Link>
+            ) : (
+              <Link href="/blog" prefetch={true} className="coolButton">
+                Blog
+              </Link>
+            )}
+          </motion.ul>
+        </div>
+      </nav>
     </header>
   );
 };
